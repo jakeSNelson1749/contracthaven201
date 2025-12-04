6107e37574e437d141f4ea522f4842dd14d9b722
Jake S. Nelson <jakeSNelson17@protonmail.com>
2025-12-04 12:11:05 -0600

    bug fix

--- contractHaven.java
+++ contractHaven.java
@@ -6,37 +6,40 @@ public class contractHaven{
         
         //main menu (V, C, M, S, L, Q)
         char choice = mainMenu.menu();
-        switch (choice) {
-            case 'V':
-                //view job board
-                jobBoard.printJobBoard();
-                System.out.println("\n");
-                choice = mainMenu.menu();
-                break;
-            case 'C':
-                //create new listing
-                utils.clearScreen();
-                //function to create new listing
-                break;
-            case 'M':
-                //access account
-                break;
-            case 'S':
-                //search
-                break;
-            case 'L':
-                //logout
-                utils.clearScreen();
-                security.clearLoginData();
-                security.login();
-                choice = mainMenu.menu();
-                break;
-            case 'Q':
-                //quit
-                System.out.println("Thank you for using Contract Haven!");
-                System.exit(0);
-            default:
-                System.out.println("Error: mainMenu invalid choice");
+        while(choice != 'Q'){
+            switch (choice) {
+                case 'V':
+                    //view job board
+                    utils.clearScreen();
+                    jobBoard.printJobBoard();
+                    System.out.println("\n");
+                    choice = mainMenu.menu();
+                    break;
+                case 'C':
+                    //create new listing
+                    utils.clearScreen();
+                    //function to create new listing
+                    break;
+                case 'M':
+                    //access account
+                    break;
+                case 'S':
+                    //search
+                    break;
+                case 'L':
+                    //logout
+                    utils.clearScreen();
+                    security.clearLoginData();
+                    security.login();
+                    choice = mainMenu.menu();
+                    break;
+                case 'Q':
+                    //quit
+                    System.out.println("Thank you for using Contract Haven!");
+                    System.exit(0);
+                default:
+                    System.out.println("Error: mainMenu invalid choice");
+            }
         }
     }
 }
